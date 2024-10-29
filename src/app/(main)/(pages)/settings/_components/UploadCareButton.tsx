1cef355aec1375ad73447fdce4fadbf3fd0eb27e
Mykyta0110 <mykyta603@gmail.com>
2024-10-29 11:55:41 +0200

    fixed function onUpload for change profile img

--- src/app/(main)/(pages)/settings/_components/UploadCareButton.tsx
+++ src/app/(main)/(pages)/settings/_components/UploadCareButton.tsx
@@ -8,18 +8,31 @@ type Props = {
   onUpload: (e: string) => any
 }
 
-
 const UploadCareButton = ({ onUpload }: Props) => {
+
   const router = useRouter()
 
+  
+  
+  const handleFileUpload = (outputState: any) => {
+    const { successEntries } = outputState;
+
+    if (successEntries && successEntries.length > 0) {
+      const fileUrl = successEntries[0].cdnUrl;
+      if (fileUrl) {
+        onUpload(fileUrl);
+        router.refresh();
+      }
+    }
+  }
+
   return (
-    <div>
-     <FileUploaderRegular
-         sourceList="local, url, camera, dropbox"
-         classNameUploader="uc-light"
-         pubkey="6157eb79ba28ac7f49de"
-      />
-    </div>
+    <FileUploaderRegular
+      sourceList="local, url, camera, dropbox"
+      classNameUploader="uc-light"
+      pubkey="6157eb79ba28ac7f49de"
+      onChange={handleFileUpload}
+    />
   )
 }
 
